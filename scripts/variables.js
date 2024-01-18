d8eb5b31b14e8278a37f536d8b21183b0bc746ef
Simran <200555884@student.georgianc.on.ca>
2024-01-18 06:17:06 +0530

    first commit

---
+++ scripts/variables.js
@@ -0,0 +1,23 @@
+/* STEP 1: Declare variables
+	Use a-z, 0-9, A-Z and underscores, avoiding reserved names
+	All program variables are generally declared at the beginning (called 'hoisting') */
+
+    /* STEP 2: Initialize variables
+	Don't confuse a variable with no value with one that doesn't exist at all */
+
+    /* STEP 3: Updating variables
+	Once a variable has been declared, this can be done any time */
+
+    // STEP 4: Variable types
+    // Numbers
+
+    // Strings (use '' or "")
+
+    // Booleans
+
+    // Arrays
+
+    // Objects
+
+    // STEP 5: Typing (JavaScript is a loosely-typed language)
+   
